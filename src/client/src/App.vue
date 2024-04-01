2f620d39e30b6525c0a7b143f85b4c5aa021e011
ardaltinay <arda.altinay@ozguryazilim.com.tr>
2024-04-01 17:50:31 +0300

    added pom.xml maven plugin settings for vuejs integration

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,14 +1,14 @@
 <template>
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <InputForm />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InputForm from './components/InputForm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    InputForm
   }
 }
 </script>
---
+++ src/client/src/components/InputForm.vue
@@ -0,0 +1,41 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input type="text" placeholder="Longitude" v-model="longitude">
+    <input type="text" placeholder="Latitude" v-model="latitude">
+    <input type="text" placeholder="Radius" v-model="radius">
+    <button type="submit">Search</button>
+  </form>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  name: 'InputForm',
+  data() {
+    return {
+      longitude: '',
+      latitude: '',
+      radius: ''
+    }
+  },
+  methods: {
+    handleSubmit() {
+      axios.get('/api/places/nearby', {
+        params: {
+          longitude: this.longitude,
+          latitude: this.latitude,
+          radius: this.radius
+        }
+      })
+      .then(res => res.json())
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
